b8093f936412f0508c41bc2b4a793a99bb7e8b49
Joel Kinman <joel.kinman@gmail.com>
2019-06-03 13:08:24 -0700

    add dist to express

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-
+app.use(express.static(__dirname + '/dist'));
+console.log('__dirname', __dirname)
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
